6fd8f27638d07a0184ae1e585c0ecdd88b1f273d
Nick Miyake <nmiyake@palantir.com>
2026-05-15 10:12:47 -0700

    Update the amalgomated version of golangci-lint

--- generated_src/golangcilint/internal/github.com/golangci/golangci-lint/v2/pkg/golinters/rowserrcheck/rowserrcheck.go
+++ generated_src/golangcilint/internal/github.com/golangci/golangci-lint/v2/pkg/golinters/rowserrcheck/rowserrcheck.go
@@ -1,7 +1,7 @@
 package rowserrcheck
 
 import (
-	"github.com/jingyugao/rowserrcheck/passes/rowserr"
+	"github.com/golangci/rowserrcheck/passes/rowserr"
 
 	"github.com/palantir/godel-distgo-asset-dist-golangci-lint/generated_src/golangcilint/internal/github.com/golangci/golangci-lint/v2/pkg/config"
 	"github.com/palantir/godel-distgo-asset-dist-golangci-lint/generated_src/golangcilint/internal/github.com/golangci/golangci-lint/v2/pkg/goanalysis"
